12ecc7d725d330c640e537d77f7a7b0ef66cc9fc
khamitov527 <khamitov527@gmail.com>
2022-01-14 10:00:38 -0500

    User can add new comment

--- parstagram/FeedViewController.swift
+++ parstagram/FeedViewController.swift
@@ -12,10 +12,13 @@ import MessageInputBar
 
 class FeedViewController: UIViewController, UITableViewDelegate, UITableViewDataSource, MessageInputBarDelegate {
 
+    
     @IBOutlet var tableView: UITableView!
     let commentBar = MessageInputBar()
     var showsCommentBar = false
     var posts = [PFObject]()
+    var selectedPost: PFObject!
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -33,11 +36,13 @@ class FeedViewController: UIViewController, UITableViewDelegate, UITableViewData
         center.addObserver(self, selector: #selector(keyboardWillBeHidden(note:)), name: UIResponder.keyboardWillHideNotification, object: nil)
     }
     
+    
     @objc func keyboardWillBeHidden(note: Notification){
         commentBar.inputTextView.text = nil
         showsCommentBar = false
         becomeFirstResponder()
     }
+    
     
     override func viewDidAppear(_ animated: Bool) {
         super.viewDidAppear(animated)
@@ -55,20 +60,42 @@ class FeedViewController: UIViewController, UITableViewDelegate, UITableViewData
         }
     }
     
+    
     func messageInputBar(_ inputBar: MessageInputBar, didPressSendButtonWith text: String) {
+        
+        let comment = PFObject(className: "Comments")
+        comment["text"] = text
+        comment["post"] = selectedPost
+        comment["author"] = PFUser.current()!
+        
+        selectedPost.add(comment, forKey: "comments")
+        
+        selectedPost.saveInBackground { success, error in
+            if success{
+                print("Comment saved!")
+            } else {
+                print("Error saving comment!")
+            }
+        }
+        
+        tableView.reloadData()
+        
         commentBar.inputTextView.text = nil
         showsCommentBar = false
         becomeFirstResponder()
         commentBar.inputTextView.resignFirstResponder()
     }
     
+    
     override var inputAccessoryView: UIView?{
         return commentBar
     }
     
+    
     override var canBecomeFirstResponder: Bool{
         return showsCommentBar
     }
+    
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         let post = posts[section]
@@ -77,9 +104,11 @@ class FeedViewController: UIViewController, UITableViewDelegate, UITableViewData
         return comments.count + 2
     }
     
+    
     func numberOfSections(in tableView: UITableView) -> Int {
         posts.count
     }
+    
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         
@@ -122,6 +151,7 @@ class FeedViewController: UIViewController, UITableViewDelegate, UITableViewData
         }
     }
     
+    
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         let post = posts[indexPath.section]
         let comments = (post["comments"] as? [PFObject]) ?? []
@@ -132,7 +162,7 @@ class FeedViewController: UIViewController, UITableViewDelegate, UITableViewData
             commentBar.inputTextView.becomeFirstResponder()
         }
         
-        
+        selectedPost = post
     }
     /*
     // MARK: - Navigation
@@ -143,5 +173,17 @@ class FeedViewController: UIViewController, UITableViewDelegate, UITableViewData
         // Pass the selected object to the new view controller.
     }
     */
+    
+    @IBAction func onLogoutButton(_ sender: Any) {
+        PFUser.logOut()
+        
+        let main = UIStoryboard(name: "Main", bundle: nil)
+        let loginViewController = main.instantiateViewController(withIdentifier: "LoginViewController")
+        guard let windowScene = UIApplication.shared.connectedScenes.first as? UIWindowScene, let delegate = windowScene.delegate as? SceneDelegate else {return}
+        
+        delegate.window?.rootViewController = loginViewController
+        
+    }
+    
 
 }
